51750c6a6d0ae2d6ce596311731002267b7c81d4
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2022-10-22 23:54:55 +0530

    added layout for components

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='layout-footer'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='layout-footer'>Header</div>
+  )
+}
+
+export default Header

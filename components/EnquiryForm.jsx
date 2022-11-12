6b78085fd8a0930c37bf2dff95756d95f7d27485
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2022-11-12 21:57:56 +0530

    displayed enquiry form data into Modal

--- components/EnquiryForm.jsx
+++ components/EnquiryForm.jsx
@@ -2,12 +2,39 @@ import React from "react";
 import Image from "next/image";
 import MainLogo from "../public/logo_firstpackers&movers.png";
 import { FiSend } from "react-icons/fi";
-import "tw-elements";
+import { useState } from "react";
 
 const EnquiryForm = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    contactno: "",
+    budget: "",
+    items: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowModal(true);
+  };
+
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div>
-      <form class="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-white">
+      <form
+        onSubmit={handleSubmit}
+        class="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-white"
+      >
         <h1 class="text-2xl font-bold dark:text-black">Send a message</h1>
 
         <label
@@ -17,8 +44,11 @@ const EnquiryForm = () => {
           Full name<span class="text-red-500 dark:text-red">*</span>
         </label>
         <input
+          id="fullName"
+          value={formData.fullName}
+          onChange={handleChange}
           type="text"
-          name="fullname"
+          name="fullName"
           class="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-black-500 font-normal text-black"
           required
         />
@@ -30,6 +60,9 @@ const EnquiryForm = () => {
           E-mail<span class="text-red-500">*</span>
         </label>
         <input
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
           type="email"
           name="email"
           class="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1font-normal ring-black-500 font-normal text-black"
@@ -43,8 +76,11 @@ const EnquiryForm = () => {
           Contact number: <span class="text-red-500">*</span>
         </label>
         <input
+          id="contactno"
+          value={formData.contactno}
+          onChange={handleChange}
           type="tel"
-          name="phone"
+          name="contactno"
           class="bg-transparent border-b py-2 pl-4 font-normal focus:outline-none focus:rounded-md focus:ring-1 ring-black-500 text-black"
           pattern="[7-9]{1}[0-9]{9}"
           required
@@ -58,11 +94,15 @@ const EnquiryForm = () => {
         </label>
 
         <select
-          name="budget"
+          required
           id="budget"
+          value={formData.budget}
+          onChange={handleChange}
+          name="budget"
           class="bg-transparent border-b py-2 pl-4 font-normal focus:outline-none focus:rounded-md focus:ring-1 ring-black-500 text-black"
         >
-          <option value="6000+"> more than 6000</option>
+          <option value="choose a valid option"> Please choose an option</option>
+          <option value="less than 6000"> less than 6000</option>
           <option value="7000-10000">7000-10000</option>
           <option value="10000-15000">10000-15000</option>
           <option value="15000+">more than 15000</option>
@@ -75,14 +115,16 @@ const EnquiryForm = () => {
           Items
         </label>
         <textarea
+          id="items"
+          value={formData.items}
+          onChange={handleChange}
           name="items"
           class="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-black-500 font-normal text-black"
         ></textarea>
         <div class="flex flex-row items-center justify-start">
           <button
             class="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center"
-            data-bs-toggle="modal"
-            data-bs-target="#exampleModalCenteredScrollable"
+            type="submit"
           >
             Send
             <FiSend className=" ml-2 text-1xl cursor-pointer color-white hover:text-blue-600" />
@@ -96,70 +138,69 @@ const EnquiryForm = () => {
 
       {/* Modal starts */}
 
-      <div
-        class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-        id="exampleModalCenteredScrollable"
-        tabindex="-1"
-        aria-labelledby="exampleModalCenteredScrollable"
-        aria-modal="true"
-        role="dialog"
-      >
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable relative w-auto pointer-events-none">
-          <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-            <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-              <h5
-                class="text-xl font-medium leading-normal text-gray-800"
-                id="exampleModalCenteredScrollableLabel"
-              >
-                Modal title
-              </h5>
-              <button
-                type="button"
-                class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="modal-body relative p-4">
-              <p>
-                This is some placeholder content to show a vertically centered
-                modal. We've added some extra copy here to show how vertically
-                centering the modal works when combined with scrollable modals.
-                We also use some repeated line breaks to quickly extend the
-                height of the content, thereby triggering the scrolling. When
-                content becomes longer than the predefined max-height of modal,
-                content will be cropped and scrollable within the modal.
-              </p>
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <p>Just like that.</p>
-            </div>
-            <div class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-              <button
-                type="button"
-                class="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button
-                type="button"
-                class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1"
-              >
-                Save changes
-              </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Enquiry Details</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Full Name:{" "}
+                    <span className="text-black ">{formData.fullName}</span>
+                  </p>
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Email: <span className="text-black ">{formData.email}</span>
+                  </p>
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Contact:{" "}
+                    <span className="text-black ">{formData.contactno}</span>
+                  </p>
+
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Budget:{" "}
+                    <span className="text-black ">{formData.budget}</span>
+                  </p>
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Items: <span className="text-black ">{formData.items}</span>
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Send Enquiry
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{html,js}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("tw-elements/dist/plugin")],
+  plugins: [],
 };

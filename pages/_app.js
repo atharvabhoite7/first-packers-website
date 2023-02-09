9966de797b38f396778bd272c401f1f1d5ff455c
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-02-09 11:23:33 +0530

    added Google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,35 @@
-import Header from '../components/Header'
-import '../styles/globals.css'
-import Footer from '../components/Footer'
+import Header from "../components/Header";
+import "../styles/globals.css";
+import Footer from "../components/Footer";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return(
-  <>
-  <Header/>
-  <Component {...pageProps} />
-  <Footer/>
-  </>
-  )
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-4QWLCVP92X"
+      />
+
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-4QWLCVP92X', {
+        page_path: window.location.pathname,
+        });
+        `,
+        }}
+      />
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
